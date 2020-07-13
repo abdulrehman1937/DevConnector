1c593c6152fe848b7279f2c5b423e7c631433308
Abdul Rehman <abdulrehman1937@hotmail.com>
2020-07-13 14:34:34 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => res.send('Api running'));
+app.listen(PORT, () => console.log('Server started on port ${PORT}'));
